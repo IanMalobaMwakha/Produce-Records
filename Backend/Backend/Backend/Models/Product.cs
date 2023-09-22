138c488cec4b21150ea4701e9c82627f82c8fd8a
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-09-22 19:50:36 +0300

    - Created the Models Folder - Created class Product with appropriate methods

---
+++ Backend/Backend/Backend/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string FarmerName { get; set; }
+        public string Email { get; set; }
+        public string Quantity { get; set; }
+        public long Value { get; set; }
+
+    }
+}
